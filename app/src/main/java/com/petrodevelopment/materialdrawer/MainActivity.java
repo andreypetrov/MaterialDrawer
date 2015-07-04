55f64d36390f034503e593a9d39fb91b51b9bdf9
Andrey Petrov <petrov.development@gmail.com>
2015-07-04 17:38:06 -0400

    Initial commit

---
+++ app/src/main/java/com/petrodevelopment/materialdrawer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.petrodevelopment.materialdrawer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.petrodevelopment.materialdrawerlibrary.MaterialDrawerActivity;
+
+
+public class MainActivity extends MaterialDrawerActivity {
+
+}
